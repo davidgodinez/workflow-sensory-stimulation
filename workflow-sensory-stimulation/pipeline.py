cf399fe4b9ad66ae6bcd7461fcdb733ebcc46666
davidgodinez <davidgodinez70@gmail.com>
2022-01-02 00:00:42 -0600

    Add ephys schema and RF table to pipeline

--- workflow-sensory-stimulation/pipeline.py
+++ workflow-sensory-stimulation/pipeline.py
@@ -2,6 +2,7 @@ import datajoint as dj
 from element_animal import subject
 from element_lab import lab
 from element_session import session
+from element_array_ephys import ephys
 
 from element_animal.subject import Subject
 from element_lab.lab import Source, Lab, Protocol, User, Project
@@ -25,7 +26,28 @@ subject.activate(db_prefix + 'subject', linking_module=__name__)
 
 session.activate(db_prefix + 'session', linking_module=__name__)
 
+ephys.activate(db_prefix + 'ephys', linking_module=__name__)
+
 
 # Activate "sensory-stimulation" schema ------------------------------------------------------
 
 stimulation.activate(db_prefix + 'stimulation', linking_module=__name__)
+
+# ------------- Declare table Equipment for use in element_calcium_imaging -------------
+
+@ephys.schema
+class RF(dj.Computed):
+    definition = """
+    # Receptive Fields
+    -> Ephys
+    -> Stimulus 
+    """
+     
+    class Unit(dj.Part):
+        definition = """
+        # Receptive fields
+        -> RF
+        -> Ephys.Spikes
+        ----
+        rf : longblob 
+        """
